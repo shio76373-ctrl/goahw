c2c68a63b0c6dfadd921542cf03528c9c1987a4e
shio76373-ctrl <shio76373@gmail.com>
2026-06-16 22:23:59 +0400

    fourth commit

---
+++ level 4/hw/hw3.py
@@ -0,0 +1,3 @@
+#გაასწორეთ ეს კოდი
+#input(enter your name
+input("enter your name")
---
+++ level 4/hw/hw5.py
@@ -0,0 +1,4 @@
+#მომხარებელს შეეკითხეთ სახელი და გვარი,ამის შემდეგ კი ეკრანზე გამოიტანეთ ორივე
+name = input("whats your name?")
+surname = input("whats your surname?")
+print(name + surname)
