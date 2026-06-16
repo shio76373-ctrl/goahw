eec0d08afda4ec6f1cbd33cd379251821f60b56e
shio76373-ctrl <shio76373@gmail.com>
2026-06-16 22:49:34 +0400

    fifth commit

---
+++ level 5/hw/hw6.py
@@ -0,0 +1,7 @@
+#შექმენი 3 ცვლადი name,height,age.სადაც შეიყვანთ შესაბამის ინფორმაციებს,ამის შემდეგ კი შეამომეთ ამათი მონაცაემთა ტიპები 
+name = "shio"
+height = 165
+age = 13
+print(type(name))
+print(type(height))
+print(type(age))
